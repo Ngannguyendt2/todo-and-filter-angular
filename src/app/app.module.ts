e457612bff2d956264645878009a376f6628cf47
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-10-24 13:33:03 +0700

    color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { FontSizeComponent } from './products/font-size/font-size.component';
 import {FormsModule} from '@angular/forms';
 import { PetComponent } from './products/pet/pet.component';
 import { CaculatorComponent } from './products/caculator/caculator.component';
+import { ColorComponent } from './products/color/color.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FontSizeComponent,
     PetComponent,
-    CaculatorComponent
+    CaculatorComponent,
+    ColorComponent
   ],
   imports: [
     BrowserModule,
